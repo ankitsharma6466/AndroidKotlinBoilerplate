d8db792676dce1c5439c42e08799a408f6e95da6
Ankit Sharma <ankitsharma@Ankits-MacBook.local>
2018-01-12 20:43:09 +0530

    timber as logger addded

--- app/src/main/java/com/ankitsharma/androidkotlinboilerplate/MyApplication.kt
+++ app/src/main/java/com/ankitsharma/androidkotlinboilerplate/MyApplication.kt
@@ -3,6 +3,7 @@ package com.ankitsharma.androidkotlinboilerplate
 import com.ankitsharma.androidkotlinboilerplate.di.DaggerAppComponent
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
+import timber.log.Timber
 
 /**
  * Created by ankitsharma on 11/01/18.
@@ -10,6 +11,7 @@ import dagger.android.DaggerApplication
 class MyApplication: DaggerApplication() {
     override fun onCreate() {
         super.onCreate()
+        Timber.plant(Timber.DebugTree())
     }
 
     override fun applicationInjector(): AndroidInjector<out MyApplication> {
