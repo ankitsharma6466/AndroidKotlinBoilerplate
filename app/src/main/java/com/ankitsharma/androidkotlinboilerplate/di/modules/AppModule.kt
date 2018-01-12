2af52fe312c774034763622467029d484b5c6f4b
Ankit Sharma <ankitsharma@Ankits-MacBook.local>
2018-01-12 14:17:26 +0530

    app comtext singleton

--- app/src/main/java/com/ankitsharma/androidkotlinboilerplate/di/modules/AppModule.kt
+++ app/src/main/java/com/ankitsharma/androidkotlinboilerplate/di/modules/AppModule.kt
@@ -3,11 +3,16 @@ package com.ankitsharma.androidkotlinboilerplate.di.modules
 import android.app.Application
 import android.content.Context
 import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
 
 /**
  * Created by ankitsharma on 11/01/18.
  */
 @Module(includes = arrayOf(NetworkModule::class, ViewModelModule::class))
 class AppModule {
+
+    @Provides
+    @Singleton
     fun provideContext(application: Application): Context = application
 }
